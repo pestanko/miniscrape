792adbcec4c7d1aeecedf8664e21473127938296
Peter Stanko <peter.stanko0@gmail.com>
2022-05-27 15:59:32 +0200

    Fix cors cookie

--- scraper/web/middlewares/cors.go
+++ scraper/web/middlewares/cors.go
@@ -4,10 +4,10 @@ import "net/http"
 
 func SetupCors(targetMux http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
-		targetMux.ServeHTTP(w, req)
-
 		w.Header().Set("Access-Control-Allow-Origin", "*")
 		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
-		//w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+		w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+
+		targetMux.ServeHTTP(w, req)
 	})
 }
