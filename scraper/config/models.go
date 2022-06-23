47359eff59cf9a069088e56f65bf5a84b94303b2
Stanko, Peter <peter.stanko@oracle.com>
2022-06-23 10:11:55 +0200

    Add encoding param

--- scraper/config/models.go
+++ scraper/config/models.go
@@ -27,6 +27,7 @@ type Page struct {
 	Tags        []string       `yaml:"tags" json:"tags"`
 	Filters     FiltersConfig  `yaml:"filters" json:"filters"`
 	Command     CommandsConfig `yaml:"command" json:"command"`
+	Encoding    string 		   `yaml:"encoding" json:"encoding"`
 }
 
 type CommandsConfig struct {
