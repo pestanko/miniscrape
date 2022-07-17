91dd923873296a4644c97b0936e39d89bf6b9349
Peter Stanko <peter.stanko0@gmail.com>
2022-07-17 10:02:31 +0200

    add update_cron.sh

---
+++ ops/update_cron.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+project_dir="$HOME/src/miniscrape"
+repo_url="https://github.com/pestanko/miniscrape.git"
+old_dir=$PWD
+
+if [ ! -e "$project_dir" ]; then
+  mkdir -p "$project_dir"
+  git clone "$repo_url" "$project_dir"
+fi
+
+
+cd $project_dir
+
+git pull
+go build
+
+cd old_dir
