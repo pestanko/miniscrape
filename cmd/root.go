f113efcc022e0c096b7ba82b3b09d54adc3c55e9
Peter Stanko <peter.stanko0@gmail.com>
2022-12-30 17:30:11 +0100

    Add the taskfile to the project

--- cmd/root.go
+++ cmd/root.go
@@ -19,16 +19,8 @@ var (
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "miniscrape",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
-	//	Run: func(cmd *cobra.Command, args []string) { },
+	Short: "A miniscrape is an application to scrape the applications",
+	Long:  `A miniscrape is an application to scrape the applications`,
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
