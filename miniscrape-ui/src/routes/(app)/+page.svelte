4c9043a1f966985ab221a0b9a9dfc98b243c8cfc
Peter Stanko <peter.stanko0@gmail.com>
2024-04-14 15:09:28 +0200

    feat(ui): Fix the flex-wrap

--- miniscrape-ui/src/routes/(app)/+page.svelte
+++ miniscrape-ui/src/routes/(app)/+page.svelte
@@ -4,17 +4,17 @@
 	export let data: {
 		tags: string[];
 	} = {
-        tags: [],
-    };
+		tags: [],
+	};
 </script>
 
 <main>
 	<h1 class="text-3xl leading-10">Tags</h1>
-	<div class="flex flex-row gap-2">
+	<div class="flex flex-row gap-2 flex-wrap">
 		{#each data.tags as tag}
 			<Card href="/pages?t={tag}">
 				<h3 class="text-2xl">{tag}</h3>
-                <p class="text-xs">Show pages with tag <strong>{tag}</strong></p>
+				<p class="text-xs">Show pages with tag <strong>{tag}</strong></p>
 			</Card>
 		{/each}
 	</div>
